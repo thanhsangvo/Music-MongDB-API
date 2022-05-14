fe59c2f7481a55576f40eedd524ab5fbf26dcf92
thanhsangvo <39442495+thanhsangvo@users.noreply.github.com>
2022-05-14 20:07:01 +0700

    add update

--- public/main.js
+++ public/main.js
@@ -4,23 +4,81 @@ const deleteButton = document.querySelectorAll(".delete")
 for (let i = 0; i < deleteButton.length; i++) {
 
     deleteButton[i].addEventListener('click', (_) => {
+
         fetch("/delete", {
             method: "delete",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
-                name: `${ deleteButton[i].value }`
+                name: `${deleteButton[i].value}`
             })
         })
+            .then((res) => {
+                console.log(`ahihi' ${res}`);
+                if (res.ok)
+                    return res.json();
+            })
+            .then((response) => {
+                console.log(response);
+                window.location.reload(true);
+            });
+        deleteButton.removeEventListener('click', (_) => { })
+
+    }, { once: false });
+}
+
+const newTabButton = document.querySelectorAll(".newTab")
+
+for (let i = 0; i < newTabButton.length; i++) {
+
+    newTabButton[i].addEventListener('click', (_) => {
+
+        fetch("/edit", {
+            method: "post",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                id: `${newTabButton[i].value}`
+            })
+        })
+            .then((res) => {
+                console.log(res);
+                if (res.ok)
+                    return res.json();
+            })
+            .then((response) => {
+                console.log(response);
+                // window.location.reload(true);
+                window.open(`/edit/${response}`)
+            });
+
+    }, { once: false });
+}
+
+const update = document.getElementById('formUpdate')
+update.addEventListener('submit', (_) => {
+
+    fetch('/update', {
+        method: 'put',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            id: document.getElementById("inputId").value,
+            name: document.getElementById("inputName").value,
+            musicURL: document.getElementById("inputMusicURL").value,
+            imgURL: document.getElementById("inputImgURL").value
+        })
+    })
         .then((res) => {
-            console.log(`ahihi' ${res}`);
-            if (res.ok) 
-            return res.json();
+            console.log(res);
+            if (res.ok)
+                return res.json();
         })
         .then((response) => {
             console.log(response);
-            window.location.reload(true);
+            alert(response)
+            // window.location.reload(true);
+            // window.open(`/edit/${response}`)
         });
-        deleteButton.removeEventListener('click', (_) => {} )
-    
-    }, {once: false});
-}
+
+        update.removeEventListener('submit', (_) => { })
+
+}, { once: false })
+
--- server.js
+++ server.js
@@ -31,23 +31,22 @@ const uri = process.env.MONGODB_URI;
 // console.log(URI);
 mongoose.connect(URI,
     err => {
-        if(err) throw err;
+        if (err) throw err;
         console.log('connected to MongoDB')
     }
 );
 
 app.get('/', (req, res) => {
-
     Music.find({}, (error, music) => {
-            // console.log(music)
-            res.render('index.ejs', { quotes: music})
-        }).sort({id: 1})
-    }
+        // console.log(music)
+        res.render('index.ejs', { quotes: music })
+    }).sort({ id: 1 })
+}
 )
 
 app.post('/addData', (req, res) => {
     const music = new Music({
-        id : req.body.id,
+        id: req.body.id,
         name: req.body.name,
         musicURL: req.body.musicURL,
         imgURL: req.body.imgURL,
@@ -71,7 +70,7 @@ app.get('/fetchAll', (req, res) => {
     })
 })
 
-app.get('/id/:id', (req, res) => {
+app.get('/search/:id', (req, res) => {
     const query = { id: parseInt(req.params.id) };
 
     Music.findOne(query, (error, music) => {
@@ -79,15 +78,49 @@ app.get('/id/:id', (req, res) => {
     })
 })
 
+app.post('/edit', (req, res) => {
+
+    const query = { id: req.body.id }
+    res.json(query.id)
+})
+
+app.get('/edit/:id', (req, res) => {
+    const query = { id: parseInt(req.params.id) };
+    Music.findOne(query, (error, music) => {
+        res.render('edit.ejs', { quotes: music })
+    })
+})
+
+app.put('/update', (req, res) => {
+    // console.log(req.body)
+    Music.findOneAndUpdate(
+        { id: req.body.id },
+        {
+            $set: {
+                id: req.body.id,
+                name: req.body.name,
+                musicURL: req.body.musicURL,
+                imgURL: req.body.imgURL,
+            }
+        },
+        {
+            upsert: true
+        }
+    )
+        .then(result => {
+            res.json('Tao đã cập nhật')
+        })
+        .catch(error => console.error(error))
+})
+
 app.delete('/delete', (req, res) => {
     const query = { name: req.body.name };
-    
-    console.log(query);
-
+    // console.log(query);
     Music.deleteOne(query).then(result => {
         res.json('Deleted Success')
-    }).catch(error => console.error(error))
+    })
+        .catch(error => console.error(error))
 })
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
